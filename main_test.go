f597a1ffa200c51564548fcf219e7deb7014a8ea
BrightFate <71946569+BrightFate@users.noreply.github.com>
2021-06-14 08:58:43 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -2,6 +2,10 @@ package main
 
 import "testing"
 
+func helloworld() string {
+	return "Hello World!!"
+}
+
 func TestHelloWorld(t *testing.T) {
 	if helloworld() != "Hello World!!" {
 		t.Fatal("Test fail")
